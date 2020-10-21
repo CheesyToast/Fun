b536bb37272e069500a6f01c872400584e2349a6
Jimmy <huan3978@gmail.com>
2020-10-21 08:18:03 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ import asyncio
 from discord.ext import commands
 
 TOKEN = "NzY4NDQwNjY1OTU5ODI1NDA5.X5AgIw.1wvWUBOlAfop8AQq6-QqpSTrci0"
-intents = discord.Intents.default()
+intents = discord.Intents.all()
 intents.members = True  
 
 bot = commands.Bot(command_prefix= "+" , intents=intents)
