96b709257cfbe4816516ae7f3a565d684ad82bd2
Jimmy <huan3978@gmail.com>
2020-10-21 08:02:10 -0400

    New bot files

---
+++ Main.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+
+TOKEN = "NzY4NDQwNjY1OTU5ODI1NDA5.X5AgIw.tl5cs3OteawFjtkOJ4PxKye8E6g"
+intents = discord.Intents.default()
+intents.members = True  # Subscribe to the privileged members intent.
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event()
+async def on_ready():
+    print("Bot is online!")
+
+@bot.command()
+@commands.guild_only()
+async def ping(ctx):
+   "This commands shows the bot latency, also known as the ping of its host server"
+   embed = discord.Embed(title = "Want a Ping? Here's a:", description = (f'Pong! {round(client.latency * 1000)}ms'), color = (0xdcddde))
+   await ctx.send(embed=embed)  
+
+bot.run(TOKEN)
